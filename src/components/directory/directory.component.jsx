05e6f062b1f3d91b599baac16d850e5212416f3b
Rushi Gohil <rushigohil18@gmail.com>
2022-05-30 22:14:48 +0530

    Added react router dom library and test it

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,7 +7,6 @@ import MenuItem from "../menu-item/menu-item.component";
 class Directory extends React.Component {
     constructor() {
         super();
-
         this.state = {
             sections: [
                 {
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,6 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`${size} menu-item`} >
-        
         <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
         }}>
